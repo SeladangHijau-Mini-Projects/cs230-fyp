e62edce7843c107833130a3e60ef9130b93eb9c4
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-13 15:33:46 +0800

    Added nav bar

---
+++ components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="navbar">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <!-- nav icon -->
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <!-- collapsed menu on tablet screen or smaller -->
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <!-- nav buttons -->
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">About</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {},
+
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style scoped></style>
