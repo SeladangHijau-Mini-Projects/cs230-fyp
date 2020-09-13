5f1b8a7d644caaf7dc5391b6d86b4cd2bc883da6
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-13 16:08:47 +0800

    added MapViz component

---
+++ components/MapViz.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+export default {
+  name: 'MapViz',
+  props: {},
+
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style scoped></style>
