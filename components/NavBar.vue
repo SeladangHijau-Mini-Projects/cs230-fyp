fed1c1c90bca8f4374faf2d07c4045fe8e88ad43
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-13 15:37:31 +0800

    added navigation link

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <div id="navbar">
     <b-navbar toggleable="lg" type="dark" variant="dark">
       <!-- nav icon -->
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="/">NavBar</b-navbar-brand>
 
       <!-- collapsed menu on tablet screen or smaller -->
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -10,8 +10,8 @@
       <!-- nav buttons -->
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Home</b-nav-item>
-          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item href="/">Home</b-nav-item>
+          <b-nav-item href="/about">About</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
