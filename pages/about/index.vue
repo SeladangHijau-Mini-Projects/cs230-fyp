0c387fbda622a200d483ae8b59271b6150fdebb7
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-13 16:08:33 +0800

    added about page

---
+++ pages/about/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <b-container>
+      <!-- title -->
+      <b-row>
+        <b-col>
+          <h1>CSP650: FINAL YEAR PROJECT</h1>
+          <h3>MALAYSIA ELECTION DATA VISUALIZATION</h3>
+          <h6>USING HEXAGON TILE GRID MAP</h6>
+        </b-col>
+      </b-row>
+
+      <br />
+
+      <!-- abstract -->
+      <b-row>
+        <b-col>
+          <h3>
+            <b><u>ABSTRACT</u></b>
+          </h3>
+          <p>
+            Data visualization is a way of representing data in a form of
+            graphical representation to help the viewer to understand the data
+            that is being visualized. Other countries such as United States of
+            America, United Kingdom, Australia and India have visualization
+            their election result data using map visualization. However, most of
+            election data in Malaysia are represented in tabular format and
+            non-interactive graphs. This creates a problem for certain viewers
+            because they could not understand the context of the data. The aim
+            of this project is to represent the election result data in a
+            simplified approach using hexagon tile grid map data visualization.
+            This project utilizes HTML, CSS, Javascript and D3.js library to
+            create hexagon tile grid map data visualization that visualizing the
+            election data in Malaysia map. The system consists of three level of
+            information. The first level shows the hexagon tile grid map of
+            Malaysia. The second level contains the gender distribution of the
+            state. The third level shows the election result with winning party
+            and candidate, besides of ethnic distribution of the selected
+            parliament. This project is validated by comparing the output with
+            the given input data. This project has successfully visualized the
+            election result accurately. This visualization helps to simplify the
+            representation of the election data and the viewer can understand
+            the data easier.
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
