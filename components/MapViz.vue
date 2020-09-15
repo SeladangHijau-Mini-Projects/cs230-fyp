4fa05c3e398c5a46bbe56294030346dfa47d6f55
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-15 14:50:31 +0800

    init MapViz (tbc)

--- components/MapViz.vue
+++ components/MapViz.vue
@@ -3,16 +3,19 @@
 </template>
 
 <script>
+import * as d3 from 'd3'
+import * as d3Hexbin from 'd3-hexbin'
+
 export default {
   name: 'MapViz',
   props: {
     width: {
       type: Number,
-      required: true,
+      default: 700,
     },
     height: {
       type: Number,
-      required: true,
+      default: 500,
     },
     padding: {
       type: Number,
@@ -46,8 +49,66 @@ export default {
       electionStates: null,
     }
   },
-  methods: {
-    init() {},
+  mounted() {
+    const hexbin = d3Hexbin.hexbin().radius(this.hexagonRadius)
+    const svg = d3
+      .select('#map')
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+    const stateList = this.electionStates
+    const parliamentCoordinateList = this.electionStates.reduce(
+      (acc, state) => {
+        acc.push(
+          Object.values(state.parliaments).reduce((acc, parliament) => {
+            acc.push([parliament.coordinate.x, parliament.coordinate.y])
+
+            return acc
+          }, [])
+        )
+
+        return acc
+      },
+      []
+    )
+
+    const g = svg.selectAll('g').data(stateList)
+
+    g.enter()
+      .append('g')
+      .attr('class', 'state')
+      .attr('id', (d, i) => {
+        return stateList[i].id
+      })
+      .style('fill', (d, i) => {
+        return stateList[i].color
+      })
+      .style('stroke', (d, i) => {
+        return stateList[i].color
+      })
+      .on('click', (d, i, j) => {})
+      .selectAll('.parliament')
+      .data((d, i) => {
+        return hexbin(parliamentCoordinateList[i])
+      })
+      .enter()
+      .append('path')
+      .attr('class', 'parliament')
+      .attr('id', (d, i, j) => {
+        return Object.keys(stateList[j].parliaments)[i]
+      })
+      .attr('d', (d) => {
+        return (
+          'M' +
+          (d.x + this.padding) +
+          ',' +
+          (d.y + this.padding) +
+          hexbin.hexagon()
+        )
+      })
+
+    console.log(hexbin)
+    console.log(parliamentCoordinateList)
   },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <MapViz />
-    <h1>Home</h1>
   </div>
 </template>
 
