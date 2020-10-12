9cc450a68f59921af8e9a3d8d5e7d2a0317e288d
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-10-12 20:01:12 +0800

    fetched data from json files

--- components/MapViz.vue
+++ components/MapViz.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script>
-import * as d3 from 'd3';
-import * as d3Hexbin from 'd3-hexbin';
+// import * as d3 from 'd3';
+// import * as d3Hexbin from 'd3-hexbin';
 
 export default {
     name: 'MapViz',
@@ -26,97 +26,26 @@ export default {
             default: 10,
         },
     },
-
     async fetch() {
         this.raceSetting = await fetch(
-            `${this.baseUrl}${this.raceEndpoint}`,
+            'http://localhost:3000/race.json',
         ).then((res) => res.json());
         this.partySetting = await fetch(
-            `${this.baseUrl}${this.partyEndpoint}`,
+            'http://localhost:3000/party.json',
         ).then((res) => res.json());
-        this.electionStates = await fetch(
-            `${this.baseUrl}${this.stateEndpoint}`,
+        this.stateSetting = await fetch(
+            'http://localhost:3000/state.json',
         ).then((res) => res.json());
     },
+
     data() {
         return {
-            baseUrl: process.env.BACKEND_SERVICE_URL || 'http://localhost:3000',
-            raceEndpoint: '/races',
-            partyEndpoint: '/parties',
-            stateEndpoint: '/states',
             raceSetting: null,
             partySetting: null,
-            electionStates: null,
+            stateSetting: null,
         };
     },
-    mounted() {
-        const hexbin = d3Hexbin.hexbin().radius(this.hexagonRadius);
-        const svg = d3
-            .select('#map')
-            .append('svg')
-            .attr('width', this.width)
-            .attr('height', this.height);
-        const stateList = this.electionStates;
-        const parliamentCoordinateList = this.electionStates.reduce(
-            (acc, state) => {
-                acc.push(
-                    Object.values(state.parliaments).reduce(
-                        (acc2, parliament) => {
-                            acc2.push([
-                                parliament.coordinate.x,
-                                parliament.coordinate.y,
-                            ]);
-
-                            return acc2;
-                        },
-                        [],
-                    ),
-                );
-
-                return acc;
-            },
-            [],
-        );
-
-        const g = svg.selectAll('g').data(stateList);
-
-        g.enter()
-            .append('g')
-            .attr('class', 'state')
-            .attr('id', (d, i) => {
-                return stateList[i].id;
-            })
-            .style('fill', (d, i) => {
-                return stateList[i].color;
-            })
-            .style('stroke', (d, i) => {
-                return stateList[i].color;
-            })
-            .on('click', (d, i, j) => {})
-            .selectAll('.parliament')
-            .data((d, i) => {
-                return hexbin(parliamentCoordinateList[i]);
-            })
-            .enter()
-            .append('path')
-            .attr('class', 'parliament')
-            .attr('id', (d, i) => {
-                console.log(this); // TODO: debug error here (cannot get index of parent node)
-                console.log(this.parentNode);
-                const j = +this.parentNode.getAttribute('data-index');
-                console.log(stateList[j]);
-                return Object.keys(stateList[j].parliaments)[i];
-            })
-            .attr('d', (d) => {
-                return (
-                    'M' +
-                    (d.x + this.padding) +
-                    ',' +
-                    (d.y + this.padding) +
-                    hexbin.hexagon()
-                );
-            });
-    },
+    mounted() {},
 };
 </script>
 
