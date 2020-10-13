bfe22db7fa84b941902edcc9456e0047428626a1
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-10-13 12:01:08 +0800

    able to fetch data from `static` folder

--- components/MapViz.vue
+++ components/MapViz.vue
@@ -3,49 +3,33 @@
 </template>
 
 <script>
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 // import * as d3Hexbin from 'd3-hexbin';
 
 export default {
     name: 'MapViz',
-    props: {
-        width: {
-            type: Number,
-            default: 700,
-        },
-        height: {
-            type: Number,
-            default: 500,
-        },
-        padding: {
-            type: Number,
-            default: 100,
-        },
-        hexagonRadius: {
-            type: Number,
-            default: 10,
-        },
-    },
-    async fetch() {
-        this.raceSetting = await fetch(
-            'http://localhost:3000/race.json',
-        ).then((res) => res.json());
-        this.partySetting = await fetch(
-            'http://localhost:3000/party.json',
-        ).then((res) => res.json());
-        this.stateSetting = await fetch(
-            'http://localhost:3000/state.json',
-        ).then((res) => res.json());
-    },
-
     data() {
         return {
+            width: 700,
+            height: 500,
+            padding: 100,
+            hexagonRadius: 10,
             raceSetting: null,
             partySetting: null,
             stateSetting: null,
         };
     },
-    mounted() {},
+
+    async mounted() {
+        await this.fetchData();
+    },
+    methods: {
+        async fetchData() {
+            this.raceSetting = await d3.json('./race.json');
+            this.partySetting = await d3.json('./party.json');
+            this.stateSetting = await d3.json('./state.json');
+        },
+    },
 };
 </script>
 
