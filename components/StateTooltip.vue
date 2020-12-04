1df3dc35dcfd9deceeb76bbcdc531c361c846f88
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2020-12-05 00:27:40 +0800

    feat: added Statetooltip component

---
+++ components/StateTooltip.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tooltip">tooltip</div>
+</template>
+
+<script>
+export default {
+    name: 'StateTooltip',
+    props: {},
+
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style scoped></style>
