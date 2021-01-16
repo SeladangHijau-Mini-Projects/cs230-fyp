51893a1131c795086c743255de58f7d1906ec0dd
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-01-16 17:35:14 +0800

    changed map UI

--- components/MapViz.vue
+++ components/MapViz.vue
@@ -1,23 +1,33 @@
 <template>
-    <div>
-        <svg id="map" :height="height" :width="width"></svg>
-
-        <StateTooltip
-            id="state-tooltip"
-            :style="'position: absolute; opacity: ' + (showTooltip ? '1' : '0')"
-            :candidate-result-list="candidateResultList"
-        />
-    </div>
+    <b-container fluid>
+        <b-row>
+            <b-col md="8">
+                <svg id="map" :height="height" :width="width"></svg>
+            </b-col>
+            <b-col md="4">
+                <CandidateResult
+                    v-for="(candidate, index) in candidateResultList"
+                    :key="index"
+                    class="candidate-result"
+                    :name="candidate.name"
+                    :party-color="partyColor(candidate.partyId)"
+                    :party-icon-src="candidate.partyIcon"
+                    :voter="candidate.voter"
+                    :vote="candidate.vote"
+                />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
 import * as d3 from 'd3';
 import * as d3Hexjson from 'd3-hexjson';
-import { StateTooltip } from './StateTooltip';
+import { CandidateResult } from './CandidateResult';
 
 export default {
     name: 'MapViz',
-    components: { StateTooltip },
+    components: { CandidateResult },
     data() {
         return {
             // config
@@ -34,7 +44,6 @@ export default {
             stateHexList: [],
 
             candidateResultList: [],
-            showTooltip: false,
         };
     },
 
@@ -66,7 +75,6 @@ export default {
                 this.height,
             );
             this.drawHex(this.stateHexList, _this.partySetting, 'state');
-            this.initStateTooltip();
 
             // draw parliament hex on state click
             this.stateHexList.on('click', (event, hex) => {
@@ -108,6 +116,8 @@ export default {
                 });
         },
         drawHex(hexList, partySettingList, className = null) {
+            const _this = this;
+
             hexList
                 .append('polygon')
                 .attr('points', function (hex) {
@@ -118,6 +128,23 @@ export default {
                 .attr('fill', function (hex) {
                     const winningPartyId = hex.result.partyId;
                     return partySettingList[winningPartyId].color;
+                })
+                .on('mouseover', function (hex, data) {
+                    const result = data.result;
+
+                    // TODO: need to show correct data
+                    _this.candidateResultList = [
+                        {
+                            name: result.candidateName,
+                            partyId: result.partyId,
+                            partyIcon: '',
+                            voter: result.voterNo,
+                            vote: result.voteNo,
+                        },
+                    ];
+                })
+                .on('mouseout', function (d) {
+                    _this.candidateResultList.length = [];
                 });
             hexList
                 .append('text')
@@ -133,49 +160,25 @@ export default {
                 hexList.attr('class', className);
             }
         },
-        initStateTooltip() {
-            const _this = this;
+        partyColor(partyId) {
+            let color = 'green';
 
-            // TODO: fix bug for blinking tooltip
-            d3.selectAll('.state')
-                .on('mouseover', function (hex, data) {
-                    const result = data.result;
+            switch (partyId) {
+                case 'pas':
+                    color = 'green';
+                    break;
+                case 'bn':
+                    color = 'blue';
+                    break;
+                case 'dap':
+                    color = 'red';
+                    break;
+                case 'others':
+                    color = 'grey';
+                    break;
+            }
 
-                    // TODO: need to show correct data
-                    _this.candidateResultList = [
-                        {
-                            name: result.candidateName,
-                            partyId: result.partyId,
-                            partyIcon: '',
-                            voter: result.voterNo,
-                            vote: result.voteNo,
-                        },
-                    ];
-                    _this.showTooltip = true;
-
-                    console.log('mouseover');
-                })
-                .on('mousemove', function (hex, data) {
-                    if (_this.showTooltip) {
-                        _this.setTooltipCoordinate(
-                            hex.clientX - 430,
-                            hex.clientY,
-                        );
-                    }
-
-                    console.log('mousemove');
-                })
-                .on('mouseout', function (d) {
-                    _this.candidateResultList.length = [];
-                    _this.showTooltip = false;
-
-                    console.log('mouseout');
-                });
-        },
-
-        setTooltipCoordinate(xCoor, yCoor) {
-            document.getElementById('state-tooltip').style.left = `${xCoor}px`;
-            document.getElementById('state-tooltip').style.top = `${yCoor}px`;
+            return color;
         },
     },
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="container">
-        <MapViz />
-    </div>
+    <b-container>
+        <b-row>
+            <b-col>
+                <MapViz />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
